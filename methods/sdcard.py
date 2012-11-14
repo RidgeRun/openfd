3345539791339240be262b702a284b5619bd064b
Jose Pablo Carballo <jose.carballo@ridgerun.com>
2012-11-14 23:46:46 +0000

    Adding installer/methods/sdcard::device_exists()

--- methods/sdcard.py
+++ methods/sdcard.py
@@ -16,12 +16,29 @@ Copyright (C) 2012 Ridgerun (http://www.ridgerun.com).
 # Imports
 # ==========================================================================
 
+import subprocess
 import rrutils
 
 # ==========================================================================
 # Private Functions
 # ==========================================================================
 
+def device_exists(device):
+    """
+    Returns true if the device exists, false otherwise.
+    """
+    
+    exists = True
+    
+    cmd = 'sudo fdisk -l ' + device + ' 2>/dev/null'
+    
+    output = subprocess.check_output(cmd, shell=True)
+    if output == "":
+        exists = False
+        
+    return exists
+    
+    
 def is_mounted(device):
     pass
 
@@ -34,4 +51,19 @@ def format_sd():
 
 
 if __name__ == '__main__':
-    pass
+
+    # Check device existence (positive test case)
+    
+    device = "/dev/sdb1"    
+    if device_exists(device):
+        print "Device " + device + " exists."
+    else:
+        print "Device " + device + " doesn't exist."
+    
+    # Check device existence (negative test case)
+        
+    device = "/dev/sdbX"    
+    if device_exists(device):
+        print "Device " + device + " exists."
+    else:
+        print "Device " + device + " doesn't exist."
