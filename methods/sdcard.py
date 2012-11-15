f621c0e89acbf1332844ec623098596528eeda44
Jose Pablo Carballo <jose.carballo@ridgerun.com>
2012-11-15 16:07:02 +0000

    Refactoring the sd card installer to be a class, instead of just functions. Starting with device_exists().

--- methods/sdcard.py
+++ methods/sdcard.py
@@ -16,46 +16,56 @@ Copyright (C) 2012 Ridgerun (http://www.ridgerun.com).
 # Imports
 # ==========================================================================
 
-import subprocess
 import rrutils
 
 # ==========================================================================
-# Private Functions
+# Public Classes
 # ==========================================================================
 
-def device_exists(device):
+class SDCardInstaller:
     """
-    Returns true if the device exists, false otherwise.
+    Class to handle SD-card operations.
     """
-    
-    exists = True
-    
-    cmd = 'sudo fdisk -l ' + device + ' 2>/dev/null'
-    
-    output = subprocess.check_output(cmd, shell=True)
-    if output == "":
-        exists = False
+
+    def __init__(self):
+        """
+        Constructor.
+        """
         
-    return exists
-    
-    
-def is_mounted(device):
-    pass
-
-# ==========================================================================
-# Public Functions
-# ==========================================================================
-
-def format_sd():
-    pass
-
+        self._config   = rrutils.config.get_global_config()
+        self._logger   = rrutils.logger.get_global_logger()
+        self._executer = rrutils.Executer()
+        
+    def device_exists(self, device):
+        """
+        Returns true if the device exists, false otherwise.
+        """
+        
+        retcode = 0
+        output  = ''
+        exists = True
+        cmd = 'sudo fdisk -l ' + device + ' 2>/dev/null'
+        
+        retcode, output = self._executer.check_output(cmd)
+        
+        if output == "":
+            exists = False
+            
+        return exists
 
 if __name__ == '__main__':
+
+    # Initialize global config and logger
+    
+    rrutils.logger.get_global_logger('sdcard-test')
+    
+    sd_installer = SDCardInstaller()
+    
 
     # Check device existence (positive test case)
     
     device = "/dev/sdb1"    
-    if device_exists(device):
+    if sd_installer.device_exists(device):
         print "Device " + device + " exists."
     else:
         print "Device " + device + " doesn't exist."
@@ -63,7 +73,9 @@ if __name__ == '__main__':
     # Check device existence (negative test case)
         
     device = "/dev/sdbX"    
-    if device_exists(device):
+    if sd_installer.device_exists(device):
         print "Device " + device + " exists."
     else:
         print "Device " + device + " doesn't exist."
+
+
