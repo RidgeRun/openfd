d12fd4807607435375a018c5b7b7896d46043e0e
Diego Benavides <diego.benavides@ridgerun.com>
2013-03-13 21:14:25 +0000

    Simplifying bootloader.py to make it more consistent with sdcard.py code

--- methods/sdcard/bootloader.py
+++ methods/sdcard/bootloader.py
@@ -34,7 +34,6 @@ RidgeRun, LLC.
 # ==========================================================================
 
 import os
-import sdcard
 import rrutils
 import sys
 
@@ -61,10 +60,6 @@ class BootloaderInstaller:
         self._dryrun      = False
         self._uflash_bin  = ''
         self._executer.set_logger(self._logger)
-        self._sd_installer = sdcard.SDCardInstaller()
-        # This flags will tell the methods to continue only if
-        # partitions info is already set.
-        self._sd_info_set = False
         
     def set_dryrun(self, dryrun):
         """
@@ -100,16 +95,12 @@ class BootloaderInstaller:
         return self._uflash_bin
         
     def flash(self, device, ubl_file, uboot_file, uboot_entry_addr,
-              uboot_load_addr,partition_index):
+              uboot_load_addr):
         """
         Flashes UBL and U-Boot to the given device, using the uflash tool.
         
         Returns true on success; false otherwise.
         """
-        m_point = self._check_partition_setup(device, partition_index)
-        if m_point == None:
-            self._logger.error('Something is wrong with device/partition setup')
-            return False
         
         if not self._uflash_bin:
             self._logger.error('No path to uflash specified')
@@ -119,6 +110,7 @@ class BootloaderInstaller:
         if uboot_entry_addr == None:
             self._logger.error("Invalid value given to uboot entry address")
             return False
+        
         uboot_load_addr = self._get_str_hex(uboot_load_addr)
         if uboot_load_addr == None:
             self._logger.error("Invalid value given to uboot load address")
@@ -156,48 +148,7 @@ class BootloaderInstaller:
                 return None
         ret_value = hex(value)
         return str(ret_value)
-    
-    def set_sd_info(self,sdcard_mmap_filename):
-        """
-        Sets the sd partitions info. 
-        """
-        if self._sd_info_set:
-            self._logger.info("SD partitions info was already set,\
-                             try unsetting it next time.")
-        if not os.path.isfile(sdcard_mmap_filename):
-            self._logger.error('Unable to find ' + sdcard_mmap_filename)
-            return False
-        if not self._sd_installer.read_partitions(sdcard_mmap_filename):
-            self._logger.error('Failed to read partitions info')
-            return False
-        self._logger.info("Sd partitions info successfully setted.")
-        self._sd_info_set = True
-        return True
-    
-    def unset_sd_info(self):
-        """
-        Unsets the _sd_info_set flag for setting new info.
-        """
-        self._sd_info_set = False
-    
-    def set_workdir(self,workdir):
-        """
-        Sets the path to the directory where to create temporary files
-        and also mount devices.
-        """
-        if os.path.isdir(workdir):
-            self._workdir = workdir
-            return True
-        else:
-            self._logger.error("Error! "+workdir+" is not a directory.")
-            return False
-    
-    def get_workdir(self):
-        """
-        Gets the working directory.
-        """
-        return self._workdir
-    
+        
     def set_bootargs(self,bootargs):
         """
         Sets the boot args used when generating the uboot env file.
@@ -210,107 +161,53 @@ class BootloaderInstaller:
         """
         return self._bootargs
 
-    def install_uboot_env(self, device, partition_index,uboot_load_addr):
+    def install_uboot_env(self, mount_point, uboot_load_addr):
         """
-        Install the U-Boot environment to the given file. 
+        Install the U-Boot environment to the given mount point. 
         """
         
-        m_point = self._check_partition_setup(device, partition_index)
-        if m_point == None:
-            self._logger.error('Something is wrong with device/partition setup')
+        if not os.path.isdir(mount_point):
+            self._logger.error('Error: The mount point given does not exist.')
             return False
         
         # Here we prepare the uboot env file.
         # but write it only if we are not in dryrun.
-        uenv_file = os.path.join(self._workdir,"uEnv.txt")
-        uboot_load_addr = self._get_str_hex(uboot_load_addr)
         if not self.get_dryrun():
-            uenv = open(uenv_file, "w")
+            uenv_file = os.path.join(mount_point,"uEnv.txt")
+            uboot_load_addr = self._get_str_hex(uboot_load_addr)
+            try:
+                uenv = open(uenv_file, "w")
+            except:
+                self._logger.error('Error: Can not creat uboot env file.')
+                return False
             uenv.write("bootargs="+self._bootargs+"\n")
             uenv.write("uenvcmd=echo Running uenvcmd ...; run loaduimage;bootm " \
                        +uboot_load_addr+"\n")
             uenv.close()
         else:
-            self._logger.info("You are running in dryrun, that's why uboot env file is not generated.")
-        
-        
-        if self._executer.check_call("sudo cp " + uenv_file +" "+ m_point) != 0:
-            self._logger.error('Failed to copy ' + uenv_file + " to " + m_point)
-            return False
+            self._logger.info("You are running in dryrun, that's why uboot env \
+                                file is not generated.")
         
         return True
         
-    def install_kernel(self, device, partition_index, kernel_image):
+    def install_kernel(self, mount_point, kernel_image):
         """
         Install the Kernel on the given device.
         """
-        m_point = self._check_partition_setup(device, partition_index)
-        if m_point == None:
-            self._logger.error('Something is wrong with device/partition setup')
+
+        if not os.path.isdir(mount_point):
+            self._logger.error('Error: The mount point given does not exist.')
             return False
         
         if not os.path.isfile(kernel_image):
             self._logger.error('Error: '+kernel_image+' is not a file!.')
             return False
         
-        if self._executer.check_call("sudo cp " + kernel_image +" "+ m_point+"/uImage") != 0:
-            self._logger.error('Failed to copy ' + kernel_image + " to " +  m_point)
+        if self._executer.check_call("sudo cp " + kernel_image +" "+ mount_point+"/uImage") != 0:
+            self._logger.error('Failed to copy ' + kernel_image + " to " +  mount_point)
             return False
         
         return True
-    
-    def _check_partition_setup(self,device,partition_index):
-        """
-        This will check the sd setup so that the calling method can continue.
-        On success will return the partition mount point, otherwise it will return None.
-        """
-        # Let's check that sd info was set.
-        if not self._sd_info_set:
-            self._logger.error("Set SD Info!")
-            return None
-        # Now let's check that there is a workdir.
-        if self.get_workdir() == None:
-            self._logger.error("Set a Workdir!")
-        # We should get sure that the device exists.
-        if not self._sd_installer.device_exists(device):
-            return None
-        # Now that we know that the device exist let's get the device info.
-        dev_info = self._sd_installer.get_dev_info(device)
-        # Now it is convenient to get the real partition suffix.
-        part_suffix = self._sd_installer.get_partition_suffix(device, partition_index)
-        # Now that we have this info, let's create a mount point for the partition.
-        # For this we will use self._workdir and the real label of the partition.
-        m_point = os.path.join( self._workdir, dev_info[device+part_suffix]["label"])
-        # Here we check if the device is mounted, if not we mount it.
-        if not self._check_sd_mounted(device,part_suffix, m_point):
-            return None
-        return m_point
-    
-    def _check_sd_mounted(self,device,partition_index,m_point):
-        """
-        Checks that the given device is mounted, if not it will try to mount
-        it on self._workdir. 
-        """
-        if not self._sd_installer.device_is_mounted(device):
-            if not self._sd_installer.mount_partitions(device, self._workdir):
-                self._logger.error('Failed to mount '+device+" on "+self._workdir)
-                return False
-        
-        # Now we check that the device is mounted where we want it to be.
-        # This will only work if dryrun is set to false.
-        if not self._dryrun:
-            partition = device+self._sd_installer.get_partition_suffix(device, partition_index)
-            current_mpoint = self._sd_installer.get_mpoint(partition)
-            if m_point != current_mpoint:
-                self._logger.error('Device is mounted on '+ current_mpoint \
-                                   +' and not on '+m_point+' as expected.')
-                return False
-        else:
-            pass
-        return True
-    
-    def check_fs(self,device):
-        self._sd_installer.check_fs(device)
 
 # ==========================================================================
 # Test cases
@@ -363,27 +260,10 @@ if __name__ == '__main__':
     # you don't repartition or flash a device you don't want to.
     
     device = "/dev/sdb"
-    partition_index = 1
     bl_installer.set_dryrun(True)
-    
-    # Now it's important to set a workdir so that the logic know where to mount devices.
-    workdir = devdir + '/images'
-    bl_installer.set_workdir(workdir)
     
     bl_installer.set_uflash_bin(devdir +
        '/bootloader/u-boot-2010.12-rc2-psp03.01.01.39/src/tools/uflash/uflash')
-    
-    workdir = devdir + '/images'
-    if not bl_installer.set_workdir(devdir + '/images'):
-        print "Failed to set working directory"
-        sys.exit(-1)
-
-    # Try to set sd partitions info.
-    
-    sdcard_mmap_filename = devdir + '/images/sd-mmap.config'
-    if not bl_installer.set_sd_info(sdcard_mmap_filename):
-        print "SD partitions info could not be setted... Exiting"
-        sys.exit(-1)
       
 # ==========================================================================
 # Test cases - Unit tests
@@ -401,12 +281,14 @@ if __name__ == '__main__':
     uboot_load_addr  = '2181038080' # 0x82000000
     
     if bl_installer.flash(device, ubl_file, uboot_file, uboot_entry_addr,
-                          uboot_load_addr,partition_index):
+                          uboot_load_addr):
         print "Device " + device + " correctly flashed"
     else:
         print "Error flashing " + device
     
     # Try to install uboot env on sd.
+    
+    mount_point = '/media/boot'
     
     bl_installer.set_bootargs(" davinci_enc_mngr.ch0_output=COMPONENT \
     davinci_enc_mngr.ch0_mode=1080I-30  davinci_display.cont2_bufsize=13631488 \
@@ -415,21 +297,17 @@ if __name__ == '__main__':
     console=ttyS0,115200n8  dm365_imp.oper_mode=0  vpfe_capture.interface=1  \
     mem=83M root=/dev/mmcblk0p2 rootdelay=2 rootfstype=ext3   ")
     
-    if bl_installer.install_uboot_env(device,partition_index,uboot_load_addr):
-        print "uboot env successfully installed on " + device + str(partition_index)
+    if bl_installer.install_uboot_env(mount_point, uboot_load_addr):
+        print "uboot env successfully installed on " + mount_point
     else:
-        print "Error installing uboot env on " + device + str(partition_index)
+        print "Error installing uboot env on " + mount_point
         sys.exit(-1)
     
-    kernel_image = workdir + '/kernel.uImage'    
-    if bl_installer.install_kernel(device,partition_index,kernel_image):
-        print "Kernel successfully installed on " + device + str(partition_index)
+    kernel_image = devdir + '/images/kernel.uImage'
+    if bl_installer.install_kernel(mount_point,kernel_image):
+        print "Kernel successfully installed on " + mount_point
     else:
-        print "Error installing kernel on " + device + str(partition_index)
+        print "Error installing kernel "+ kernel_image + " on " + mount_point
         sys.exit(-1)
-    
-    # Let's check that the filesystem is ok.
-    print "Checking fs..."
-    bl_installer.check_fs(device)
     
     print "Test cases finished"
