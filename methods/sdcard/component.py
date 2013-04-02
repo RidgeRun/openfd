601c8fb9eaef635e300bec41fb122db87b40261f
Jose Pablo Carballo <jose.carballo@ridgerun.com>
2013-04-02 21:32:56 +0000

    Removing the Bootloader and Filesystem installer concepts, in favour of a single ComponentInstaller().

--- methods/sdcard/component.py
+++ methods/sdcard/component.py
@@ -12,12 +12,15 @@
 # into another programming language without prior written consent of 
 # RidgeRun, LLC.
 #
-# Bootloader related operations to support the installer.
+# Components operations to support the installer.
 #
 # ==========================================================================
 
 """
-Bootloader related operations to support the installer.
+Components operations to support the installer. Current components are:
+  - Bootloader (includes pre-bootloader)
+  - Kernel
+  - Filesystem
 
 Copyright (C) 2013 RidgeRun, LLC (http://www.ridgerun.com)
 All Rights Reserved.
@@ -27,7 +30,6 @@ LLC.  No part of this program may be photocopied, reproduced or translated
 into another programming language without prior written consent of 
 RidgeRun, LLC.
 """
-
 
 # ==========================================================================
 # Imports
@@ -41,13 +43,9 @@ import sys
 # Public Classes
 # ==========================================================================
 
-class BootloaderInstaller(object):
+class ComponentInstaller(object):
     """
-    Class to handle bootloader-related operations.
-    
-    Uses the uflash tool (Linux commmand tool specific to TI's Davinci
-    platforms) for flashing UBL (User Boot Loader), u-boot and u-boot
-    Environment in the MMC/SDcard.
+    Class to handle components-related operations.
     """
     
     def __init__(self):
@@ -65,8 +63,27 @@ class BootloaderInstaller(object):
         self._uboot_entry_addr = None
         self._uboot_load_addr = None
         self._kernel_image = None
+        self._rootfs = None
         self._executer.set_logger(self._logger)
+
+    def _get_str_hex(self, value_str):
+        """
+        Converts the string that may contain a decimal number (like 12), or
+        a hexadecimal number (like 0xC), into an all-lowercase hexadecimal
+        number (like 0xc).
+        """
         
+        if not value_str:
+            return ''
+        
+        if not value_str.upper().find('0X'):
+            try:
+                return hex(int(value_str))
+            except:
+                return ''
+        else:
+            return value_str.lower()
+
     def set_dryrun(self, dryrun):
         """
         Sets on/off the dryrun mode. In dryrun mode any commands will
@@ -113,91 +130,111 @@ class BootloaderInstaller(object):
     
     def set_uboot_file(self, uboot_file):
         """
-        Sets the path to the uboot_file.
+        Sets the path to the uboot file.
         """
         
         self._uboot_file = uboot_file
         
     def get_uboot_file(self):
         """
-        Gets the path to the uboot_file.
+        Gets the path to the uboot file.
         """
         
         return self._uboot_file
     
     def set_uboot_entry_addr(self, uboot_entry_addr):
         """
-        Sets the path to the uboot_entry_addr.
+        Sets the uboot entry address.
         """
         
         self._uboot_entry_addr = uboot_entry_addr
         
     def get_uboot_entry_addr(self):
         """
-        Gets the path to the uboot_entry_addr.
+        Gets the path to the uboot entry address.
         """
         
         return self._uboot_entry_addr
     
     def set_uboot_load_addr(self, uboot_load_addr):
         """
-        Sets the path to the uboot_load_addr.
+        Sets the path to the ubootload address.
         """
         self._uboot_load_addr = uboot_load_addr
         
     def get_uboot_load_addr(self):
         """
-        Gets the path to the uboot_load_addr.
+        Gets the path to the uboot load address.
         """
         
         return self._uboot_load_addr
     
     def set_bootargs(self,bootargs):
         """
-        Sets the boot args used when generating the uboot env file.
+        Sets the uboot environment variable "bootargs".
         """
         self._bootargs = bootargs
     
     def get_bootargs(self):
         """
-        Gets the boot args.
+        Gets the uboot environment variable "bootargs".
         """
         
         return self._bootargs
     
     def set_kernel_image(self, kernel_image):
         """
-        Sets the path to the kernel_image.
+        Sets the path to the kernel image.
         """
         
         self._kernel_image = kernel_image
         
     def get_kernel_image(self):
         """
-        Gets the path to the kernel_image.
+        Gets the path to the kernel image.
         """
         
         return self._kernel_image
     
+    def set_rootfs(self, rootfs):
+        """
+        Sets the path to rootfs. Set to None if this installation does not
+        require a rootfs, i.e. NFS will be used.
+        """
+        
+        self._rootfs = rootfs
+
+    def get_rootfs(self):
+        """
+        Gets the path to rootfs. If None, a rootfs was not specified, more
+        likely because this installation does not require rootfs, i.e. NFS
+        will be used. 
+        """
+        
+        return self._rootfs
+    
     def set_workdir(self, workdir):
         """
-        Sets the path to the workdir.
+        Sets the path to the workdir - a directory where this installer can 
+        write to files and perform other temporary operations.
         """
         
         self._workdir = workdir
         
     def get_workdir(self):
         """
-        Gets the path to the kernel_image.
+        Gets the path to the workdir - a directory where this installer can 
+        write to files and perform other temporary operations.
         """
         
         return self._workdir
     
-    def flash_uboot(self, device):
+    def install_uboot(self, device):
         """
         Flashes UBL and U-Boot to the given device, using the uflash tool.
         This method needs that uflash_bin, ubl_file, uboot_file, 
         uboot_entry_addr and uboot_load_addr to be already set.
+        
         Returns true on success; false otherwise.
         """
         
@@ -215,9 +252,11 @@ class BootloaderInstaller(object):
         
         if not self._uboot_entry_addr:
             self._logger.error('No uboot entry address specified')
+            return False
 
         if not self._uboot_load_addr:
             self._logger.error('No uboot load address specified')
+            return False
         
         uboot_entry_addr = self._get_str_hex(self._uboot_entry_addr)
         if uboot_entry_addr == None:
@@ -246,24 +285,6 @@ class BootloaderInstaller(object):
 
         return True
     
-    def _get_str_hex(self, value_str):
-        """
-        Converts the string that may contain a decimal number (like 12), or
-        a hexadecimal number (like 0Xabcd), into an all-lowercase hexadecimal
-        number (like 0xabcd).
-        """
-        
-        if not value_str:
-            return ''
-        
-        if not value_str.upper().find('0X'):
-            try:
-                return hex(int(value_str))
-            except:
-                return ''
-        else:
-            return value_str.lower()
-        
     def install_uboot_env(self, mount_point):
         """
         Installs the U-Boot environment to the given mount point. Assumes
@@ -296,7 +317,8 @@ class BootloaderInstaller(object):
             uenv = open(uenv_file, "w")
             
             uenv.write('bootargs=%s\n' % self._bootargs)
-            uenv.write('uenvcmd=echo Running uenvcmd ...; run loaduimage; '
+            uenv.write('uenvcmd=echo Running uenvcmd ...; '
+                       'run loaduimage; '
                        'bootm %s\n' % uboot_load_addr)
             uenv.close()
         
@@ -310,8 +332,9 @@ class BootloaderInstaller(object):
         
     def install_kernel(self, mount_point):
         """
-        Install the Kernel on the given device.
-        This method needs the kernel_image to be set.
+        Installs the Kernel on the given mount point.
+        
+        Returns true on success, false otherwise.
         """
         
         if not self._kernel_image:
@@ -324,6 +347,25 @@ class BootloaderInstaller(object):
             self._logger.error('Failed copying %s to %s' %
                                (self._kernel_image, mount_point))
             return False
+        
+        return True
+    
+    def install_rootfs(self, mount_point):
+        """
+        If any, installs the rootfs to the given mount point.
+        
+        Returns true on success, false otherwise.
+        """
+        
+        if self._rootfs:
+        
+            cmd = 'cd ' + self._rootfs + ' ; find . | sudo cpio -pdum ' + \
+                    mount_point
+            
+            if self._executer.check_call(cmd) != 0:
+                self._logger.error('Failed installing rootfs into %s' %
+                                   mount_point)
+                return False
         
         return True
 
@@ -369,7 +411,7 @@ if __name__ == '__main__':
     rrutils.logger.basic_config(verbose=True)
     logger = rrutils.logger.get_global_logger('sdcard-test')
     
-    bl_installer = BootloaderInstaller()
+    comp_installer = ComponentInstaller()
     
     # The following test cases will be run over the following device,
     # in the given dryrun mode, unless otherwise specified in the test case.
@@ -378,12 +420,12 @@ if __name__ == '__main__':
     # you don't repartition or flash a device you don't want to.
     
     device = "/dev/sdb"
-    bl_installer.set_dryrun(True)
+    comp_installer.set_dryrun(True)
     
     uflash_bin = devdir + \
        '/bootloader/u-boot-2010.12-rc2-psp03.01.01.39/src/tools/uflash/uflash'
     
-    bl_installer.set_uflash_bin(uflash_bin)
+    comp_installer.set_uflash_bin(uflash_bin)
 
 # ==========================================================================
 # Test cases - Unit tests
@@ -401,15 +443,15 @@ if __name__ == '__main__':
     uboot_entry_addr = '0x82000000' # 2181038080
     uboot_load_addr = '2181038080' # 0x82000000
     
-    bl_installer.set_ubl_file(ubl_file)
-    bl_installer.set_uboot_file(uboot_file)
-    bl_installer.set_uboot_entry_addr(uboot_entry_addr)
-    bl_installer.set_uboot_load_addr(uboot_load_addr)
-    bl_installer.set_workdir(workdir)
+    comp_installer.set_ubl_file(ubl_file)
+    comp_installer.set_uboot_file(uboot_file)
+    comp_installer.set_uboot_entry_addr(uboot_entry_addr)
+    comp_installer.set_uboot_load_addr(uboot_load_addr)
+    comp_installer.set_workdir(workdir)
     
     # Flash the device
     
-    if bl_installer.flash(device):
+    if comp_installer.install_uboot(device):
         print "Device " + device + " correctly flashed"
     else:
         print "Error flashing " + device
@@ -422,7 +464,7 @@ if __name__ == '__main__':
     
     mount_point = '/media/boot'
     
-    bl_installer.set_bootargs("davinci_enc_mngr.ch0_output=COMPONENT "
+    comp_installer.set_bootargs("davinci_enc_mngr.ch0_output=COMPONENT "
                               "davinci_enc_mngr.ch0_mode=1080I-30  "
                               "davinci_display.cont2_bufsize=13631488 "
                               "vpfe_capture.cont_bufoffset=13631488 "
@@ -432,7 +474,7 @@ if __name__ == '__main__':
                               "mem=83M root=/dev/mmcblk0p2 rootdelay=2 "
                               "rootfstype=ext3")
     
-    if bl_installer.install_uboot_env(mount_point):
+    if comp_installer.install_uboot_env(mount_point):
         print "Uboot env successfully installed on " + mount_point
     else:
         print "Error installing uboot env on " + mount_point
@@ -441,15 +483,29 @@ if __name__ == '__main__':
     
     tc_start(3)
     
-    # Kernel installation
+    # Kernel  installation
     
     kernel_image = devdir + '/images/kernel.uImage'
     
-    bl_installer.set_kernel_image(kernel_image)
+    comp_installer.set_kernel_image(kernel_image)
     
-    if bl_installer.install_kernel(mount_point):
+    if comp_installer.install_kernel(mount_point):
         print "Kernel successfully installed on " + mount_point
     else:
         print "Error installing kernel " + kernel_image + " on " + mount_point
+
+    # --------------- TC 4 ---------------
+    
+    tc_start(4) 
+    
+    rootfs = devdir + "/fs/fs"
+    comp_installer.set_rootfs(rootfs)
+    mount_point = "/media/rootfs"
+
+    if comp_installer.install_rootfs(mount_point):
+        print "Rootfs successfully installed on " + mount_point
+    else:
+        print "Error installing rootfs on " + mount_point
+        sys.exit(-1)
     
     print "Test cases finished"
