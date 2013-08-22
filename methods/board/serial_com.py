2e3131b0f610260e6be27699e85711fb4bcbaed1
Jose Pablo Carballo <jose.carballo@ridgerun.com>
2013-08-22 21:16:15 +0000

    Adding open_comm() to open the serial port, and other cleanups.

--- methods/board/serial_com.py
+++ methods/board/serial_com.py
@@ -37,6 +37,14 @@ import serial
 import rrutils
 
 # ==========================================================================
+# Constants
+# ==========================================================================
+
+DEFAULT_PORT = '/dev/ttyS0'
+DEFAULT_BAUDRATE = 115200
+DEFAULT_READ_TIMEOUT = 2
+
+# ==========================================================================
 # Public Classes
 # ==========================================================================
 
@@ -46,36 +54,27 @@ class SerialInstaller(object):
     pySerial.
     
     Attributes:
-        _port: A Serial port instance for serial communication. 
+        port: A Serial port instance for serial communication. None if no
+            serial port has been opened using open_comm().
+        nand_block_size: The NAND block size will be retrieved from uboot
+            by default. But if this property was manually set, uboot will
+            not be queried, unless it is manually set back to the value 0.
+        dryrun: When True, all commands will be logged, but not executed.
     """
-    
-    def __init__(self, port='/dev/ttyS0', baud=115200, timeout=2):
+        
+    def __init__(self):
         """
         Constructor.
-        
-        Args:
-            port: Device name or port number (i.e. /dev/ttyS0)
-            baud: Baud rate such as 9600 or 115200 etc
-            timeout: Set a read timeout value
-        
-        Raises:
-            SerialException: On error while opening the serial port.
         """
         
         self._logger = rrutils.logger.get_global_logger()
-        self._nand_block_size = 0 
-        
-        # Open the serial port
-        try:
-            self._port = serial.Serial(port=port,
-                                       baudrate=baud,
-                                       timeout=timeout)
-        except serial.SerialException as e:
-            self._logger.error(e)
-            raise e
+        self._executer = rrutils.executer.Executer()
+        self._executer.logger = self._logger
+        self._dryrun = False
+        self._port = None
 
     @classmethod
-    def com_error_msg(cls, port):
+    def uboot_comm_error_msg(cls, port):
         """
         Standard error message to report a failure communicating with the given
         port.
@@ -90,68 +89,48 @@ class SerialInstaller(object):
         return ('Failed to handshake with uboot.\n'
                'Be sure u-boot is active on port %s and you have terminal '
                'programs like minicom closed.' % port)
-
-    def __expect(self, response, timeout=5):
-        """
-        Expects a response from the serial port for no more than timeout
-        seconds.
-        
-        The lines read from the serial port will be stripped (\s\r\n) before
-        being compared with response.
-        
-        Args:
-            response: A string to expect in the serial port.
-            timeout: Timeout in seconds to wait for the response. 
-            
-        Returns:
-            Returns a tuple with two items. The first one is true if the
-            response was found; false otherwise. The second is the complete
-            line where the response was found, or the last line read if the
-            response wasn't found and the timeout reached. The line is
-            returned stripped (\s\r\n).
-        """
-        
-        start_time = time.time()
-        
-        found = False
-        line = ''
-        
-        while not found:
-            
-            try:
-                line = self._port.readline().strip('\s\r\n')
-            except serial.SerialException as e:
-                self._logger.error(e)
-                return False, ''
-            
-            if line.find(response) != -1:
-                found = True
-                
-            if (time.time() - start_time) > timeout:
-                break
-
-        return found, line
-
-    def uboot_sync(self):
-        """
-        Synchronizes with uboot. If successful, uboot's prompt will 
-        be ready to receive commands.
-            
-        Returns:
-            Returns true on success; false otherwise.
-        """
     
-        self._port.flush()
-        self._port.write('echo resync\n')
+    @property
+    def port(self):
+        """
+        Gets the Serial port instance. It may be None if no
+        serial port has been opened using open_comm().
+        """
         
-        ret = self.__expect('resync')[0]
+        return self._port
+    
+    def check_open_port(self):
+        """
+        Checks if the serial port has been successfully opened.
         
-        if not ret:
-            msg = SerialInstaller.com_error_msg(self._port.port)
-            self._logger.error(msg)
+        Returns:
+            Returns true if the port is opened; false otherwise.
+        """
+        
+        if self._port is None:
+            self._logger.error('No opened port (try open_comm() first)')
             return False
+        else:
+            return True
+    
+    def __set_dryrun(self, dryrun):
+        """
+        Sets on/off the dryrun mode. In dryrun mode any commands will
+        not be executed (just logged).
+        """
         
-        return True 
+        self._dryrun = dryrun
+        self._executer.dryrun = dryrun
+    
+    def __get_dryrun(self):
+        """
+        Returns true if the dryrun mode is on; false otherwise.
+        """
+        
+        return self._dryrun
+    
+    dryrun = property(__get_dryrun, __set_dryrun,
+                     doc="""Gets or sets the dryrun mode.""")
 
     def __set_nand_block_size(self, size):
         """
@@ -176,8 +155,9 @@ class SerialInstaller(object):
         if self._nand_block_size != 0:
             return self._nand_block_size
         
-        size_kb = 0
+        if self.check_open_port() is False: return 0
         
+        # Ask uboot
         self._port.write('nand info\n')
         ret, line = self.__expect('Device 0')
         if not ret:
@@ -190,12 +170,13 @@ class SerialInstaller(object):
         # old: Device 0: Samsung K9K1208Q0C at 0x2000000 (64 MB, 16 kB sector)
         # new: Device 0: NAND 256MiB 1,8V 16-bit, sector size 128 KiB
         
+        size_kb = 0
         m = re.match('.* (\d+) kb.*', line, re.IGNORECASE)
-        if m: # try the old output
+        if m:
             size_kb = int(m.group(1))
         else:
             m = re.match('.* (\d+) kib.*', line, re.IGNORECASE)
-            if m: # try the new output
+            if m:
                 size_kb = int(m.group(1))
             else:
                 self._logger.error('Unable to determine the NAND block size')
@@ -204,7 +185,113 @@ class SerialInstaller(object):
     
     nand_block_size = property(__get_nand_block_size, __set_nand_block_size, 
                            doc="""Gets or sets the NAND block size""")
+    
+    def open_comm(self, port=DEFAULT_PORT,
+                  baud=DEFAULT_BAUDRATE,
+                  timeout=DEFAULT_READ_TIMEOUT):
+        """
+        Opens the communication with the Serial port.
         
+        Args:
+            port: Device name or port number (i.e. /dev/ttyS0)
+            baud: Baud rate such as 9600 or 115200 etc
+            timeout: Set a read timeout value
+            
+        Returns:
+            Returns true on success; false otherwise.
+            
+        Raises:
+            SerialException: On error while opening the serial port.
+        """
+        
+        # Terminal line settings
+        cmd = ('stty -F %s %s intr ^C quit ^D erase ^H kill ^U eof ^Z '
+               'eol ^J start ^Q stop ^S -echo echoe echok -echonl echoke '
+               '-echoctl -istrip -icrnl -ocrnl -igncr -inlcr onlcr -opost '
+               '-isig -icanon cs8 -cstopb clocal -crtscts -ixoff -ixon '
+               '-parenb -parodd -inpck' % (port, baud))
+        
+        ret = self._executer.check_call(cmd)
+        if ret != 0:
+            self._logger.error('Couldn\'t change terminal line settings')
+            return False
+        
+        # Open the serial port
+        try:
+            self._port = serial.Serial(port=port,
+                                       baudrate=baud,
+                                       timeout=timeout)
+        except serial.SerialException as e:
+            self._logger.error(e)
+            raise e
+        
+        return True
+
+    def __expect(self, response, timeout=5):
+        """
+        Expects a response from the serial port for no more than timeout
+        seconds.
+        
+        The lines read from the serial port will be stripped (\s\r\n) before
+        being compared with response.
+        
+        Args:
+            response: A string to expect in the serial port.
+            timeout: Timeout in seconds to wait for the response. 
+            
+        Returns:
+            Returns a tuple with two items. The first one is true if the
+            response was found; false otherwise. The second is the complete
+            line where the response was found, or the last line read if the
+            response wasn't found and the timeout reached. The line is
+            returned stripped (\s\r\n).
+        """
+        
+        found = False
+        line = ''
+        start_time = time.time()
+        
+        if self.check_open_port() is False: return False, ''
+        
+        while not found:
+            
+            try:
+                line = self._port.readline().strip('\s\r\n')
+            except serial.SerialException as e:
+                self._logger.error(e)
+                return False, ''
+            
+            if line.find(response) != -1:
+                found = True
+            
+            if (time.time() - start_time) > timeout:
+                break
+
+        return found, line
+
+    def uboot_sync(self):
+        """
+        Synchronizes with uboot. If successful, uboot's prompt will 
+        be ready to receive commands.
+            
+        Returns:
+            Returns true on success; false otherwise.
+        """
+    
+        if self.check_open_port() is False: return False
+    
+        self._port.flush()
+        self._port.write('echo resync\n')
+        
+        ret = self.__expect('resync')[0]
+        
+        if not ret:
+            msg = SerialInstaller.uboot_comm_error_msg(self._port.port)
+            self._logger.error(msg)
+            return False
+        
+        return True
+    
 # ==========================================================================
 # Test cases
 # ==========================================================================
@@ -251,10 +338,15 @@ if __name__ == '__main__':
     inst = None
     port = '/dev/ttyUSB0'
     try:
-        inst = SerialInstaller(port, 115200)
-        print "Port %s opened" % port
+        inst = SerialInstaller()
+        if inst.open_comm(port='/dev/ttyUSB0',baud=115200):
+            print 'Port %s opened' % port
+        else:
+            print 'Failed to open port %s' % port
+            sys.exit(-1)
+            
     except:
-        print 'Failed to open %s' % port
+        print 'SerialException: Failed to open %s' % port
         sys.exit(-1)
     
     # --------------- TC 2 ---------------
@@ -287,3 +379,4 @@ if __name__ == '__main__':
         print "NAND block size: 0x%x" % size
     else:
         print "Failed to obtain the NAND block size"
+    
