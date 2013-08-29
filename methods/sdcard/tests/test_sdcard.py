9f3cfd3dd69394cb5c6b48b166dd076449f1f719
Jose Pablo Carballo <jose.carballo@ridgerun.com>
2013-08-29 23:29:38 +0000

    Migration checkpoint, sdcard unit tests

--- methods/sdcard/tests/test_sdcard.py
+++ methods/sdcard/tests/test_sdcard.py
@@ -29,20 +29,8 @@ from component import ComponentInstaller
 devdir = check_env.get_devdir()
 if not devdir: sys.exit(-1)
 
-# Default variables for test cases - Leo DM368
-
-uboot = 'u-boot-2010.12-rc2-psp03.01.01.39'
-uflash_bin = '%s/bootloader/%s/src/tools/uflash/uflash' % (devdir, uboot)
-ubl_file = '%s/images/ubl_DM36x_sdmmc.bin' % devdir
-uboot_file = '%s/images/bootloader' % devdir
-uboot_entry_addr = '0x82000000' # 2181038080
-uboot_load_addr = '2181038080' # 0x82000000
-kernel_image = '%s/images/kernel.uImage' % devdir
-rootfs = '%s/fs/fs' % devdir
-workdir = "%s/images" % devdir
-
 # IMPORTANT: Main test device (be careful!)
-test_device = '/dev/ttyUSB0'
+test_device = '/dev/sdb'
 
 class SDCardInstallerTestCase(unittest.TestCase):
     
@@ -53,6 +41,17 @@ class SDCardInstallerTestCase(unittest.TestCase):
         logger.setLevel(rrutils.logger.DEBUG)
         
     def setUp(self):
+
+        # Default variables for test cases - Leo DM368
+        uboot = 'u-boot-2010.12-rc2-psp03.01.01.39'
+        uflash_bin = '%s/bootloader/%s/src/tools/uflash/uflash' % (devdir, uboot)
+        ubl_file = '%s/images/ubl_DM36x_sdmmc.bin' % devdir
+        uboot_file = '%s/images/bootloader' % devdir
+        uboot_entry_addr = '0x82000000' # 2181038080
+        uboot_load_addr = '2181038080' # 0x82000000
+        kernel_image = '%s/images/kernel.uImage' % devdir
+        rootfs = '%s/fs/fs' % devdir
+        workdir = "%s/images" % devdir
         
         # Component installer
         self._comp_installer = ComponentInstaller()        
@@ -66,15 +65,51 @@ class SDCardInstallerTestCase(unittest.TestCase):
         self._comp_installer.workdir = workdir
         
         # SDCard Installer
-        
-        self._sd_installer = SDCardInstaller(self._comp_installer)
+        self._inst = SDCardInstaller(self._comp_installer)
         
     def tearDown(self):
         pass
         
-    def test_first(self):
-        self._sd_installer.mode = SDCardInstaller.MODE_SD
-        self._sd_installer.device = test_device
+    def test_device(self):
+        
+        # Change this booleans to enable/disable tests that assume some
+        # preconditions (i.e. we know in advance the device is mounted,
+        # we know the device size, etc.)
+        test_mounted = True
+        test_sizes = True
+        
+        # Device information
+        is_mounted = True
+        mounted_partitions = ['/media/rootfs_', '/media/boot'] # 'mount | grep /dev/sdb  | cut -f 3 -d " "
+        size_b = 2002780160 # sudo fdisk -l <device> | grep <device> | grep Disk | cut -f 5 -d " "
+        size_gb = 1
+        size_cyl = 243
+        
+        #   --------- Don't edit below -----------
+        
+        self._inst.mode = SDCardInstaller.MODE_SD
+        self._inst.dryrun = False
+        self._inst.interactive = False
+        
+        # Device existence
+        self._inst.device = '/dev/sdbX'
+        self.assertFalse(self._inst._device_exists())
+        self._inst.device = test_device
+        self.assertTrue(self._inst._device_exists())
+        
+        # Device mounted
+        if test_mounted:
+            self.assertEqual(self._inst._device_is_mounted(), is_mounted)
+            partitions = self._inst._get_device_mounted_partitions()
+            self.assertEqual(partitions, mounted_partitions)
+        
+        # Device sizes
+        if test_sizes:
+            self.assertEqual(self._inst._get_device_size_b(), size_b)
+            self.assertEqual(self._inst._get_device_size_gb(), size_gb)
+            self.assertEqual(self._inst._get_device_size_cyl(), size_cyl)
+            
+
         
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(SDCardInstallerTestCase)
