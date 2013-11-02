1dea0e25a2d387ce1ebca97bfe61c2e93fb932f7
Jose Pablo Carballo <jose.carballo@ridgerun.com>
2013-11-02 05:58:47 +0000

    Starting to add the concept of subparsers to group command line arguments, using ArgumentParser directly instead of rrutils.Parser. WIP....

--- installer.py
+++ installer.py
@@ -79,20 +79,40 @@ import os
 import sys
 import rrutils
 import methods
+import argparse
 
 # ==========================================================================
 # Global variables
 # ==========================================================================
 
-_options = []
-_parser  = None
+_args = []
+_args_sd = []
+_args_sd_img = []
+_args_nand = []
+_parser = None
+_parser_sd = None
+_parser_sd_img = None
+_parser_nand = None
+_subparsers = None
 _logger  = None
 
+# ==========================================================================
 # Constants
+# ==========================================================================
 
+# Modes
 MODE_SD = 'sd'
-MODE_SERIAL = 'serial'
+MODE_SD_IMG = 'sd-img'
+MODE_NAND = 'nand'
 MODE_LOOPBACK = 'loopback'
+
+# Components
+COMP_IPL = "ipl"
+COMP_BOOTLOADER = "bootloader"
+COMP_KERNEL = "kernel"
+COMP_FS = "filesystem"
+COMP_CMDLINE = "cmdline"
+COMP_BOOTCMD = "bootcmd"
 
 # ==========================================================================
 # Logging
@@ -113,257 +133,217 @@ def _init_logging():
 # ==========================================================================
 
 def _clean_exit(code=0):
-    """
-    Closes open resources and exits.
-    """
-    
-    if code == 0:
-        pass
-    else:
-        _logger.error('Exiting with code %d' % code) 
-
+    if code != 0: _logger.error('Exiting with code %d' % code)
     exit(code)
 
 def _abort_install():
-    """
-    Prints abort message, closes open resources and exits.
-    """
-    
     _logger.error('Installation aborted')
     _clean_exit(-1)
-   
+
+def _check_is_dir(dirname):
+    if not os.path.isdir(dirname):
+        _logger.error('Unable to find %s' % dirname)
+        _clean_exit(-1)
+
+def _check_is_file(filename):
+    if not os.path.isfile(filename):
+        _logger.error('Unable to find %s' % filename)
+        _clean_exit(-1)
+
+def _check_x_ok(filename):
+    if not os.access(filename, os.X_OK):
+        _logger.error('No execution permissions on %s' % filename)
+        _clean_exit(-1)
+
+def _check_is_int(val, arg):
+    try:
+        int(val)
+    except ValueError:
+        _logger.error('%s must be an integer (%s)' % (arg, val))
+        _clean_exit(-1)
+        
 # ==========================================================================
 # Command line arguments
 # ==========================================================================
 
 def _missing_arg_exit(arg):
-    """
-    Prints message indicating arg is required, prints help and exit.
-    """
-    
     _parser.print_help()
     _logger.error('Argument %s is required' % arg)
     _clean_exit(-1)
-
+    
 def _parse_args():
-    
+    global _args
     global _parser
-    global _options
+    global _subparsers
+    _parser = argparse.ArgumentParser()
+    _subparsers = _parser.add_subparsers(help="installation mode (--help available)",
+                                         dest="mode")
     
-    _parser = rrutils.Parser()
-    
-    # Required arguments
-    
-    installation_modes = MODE_SD, MODE_LOOPBACK
-    
-    _parser.add_option('-m', '--mode',
-                       help="Installation mode: % s" % 
-                       ''.join('%s' % mode for mode in installation_modes),
-                       metavar='<mode>',
-                       dest='installation_mode',
-                       required=True,
-                       choices=installation_modes)
-    
-    _parser.add_option('-f', '--mmap-config-file',
-                       help='Memory map config file',
-                       metavar='<mmap>',
-                       dest='mmap_file',
-                       required=True)
-    
-    _parser.add_option('--kernel-file',
-                       help='Path to the Kernel Image file to be installed.',
-                       metavar='<kernel_file>',
-                       dest='kernel_file',
-                       required=True)
-    
-    # Optional arguments
-    
-    _parser.add_option('-y', '--assume-yes',
+    _parser.add_argument('-y', '--assume-yes',
                        help='Automatic \'yes\' to prompts; '
-                       'run non-interactively',
+                       'runs non-interactively',
                        dest='interactive',
                        action='store_false',
                        default=True)
     
-    _parser.add_option('-v', '--verbose',
+    _parser.add_argument('-v', '--verbose',
                        help='Enable debug',
                        dest='verbose',
                        action='store_true',
                        default=False)
     
-    _parser.add_option('-q', '--quiet',
+    _parser.add_argument('-q', '--quiet',
                        help='Be as quiet as possible',
                        dest='quiet',
                        action='store_true',
                        default=False)
     
-    _parser.add_option('--dryrun',
+    _parser.add_argument('--dryrun',
                        help='Sets the dryrun mode On (shell commands will be '
                             'logged, but not executed)',
                        dest='dryrun',
                        action='store_true',
                        default=False)
+
+def _parse_sd_img_args():
+    global _parser_sd_img
+    _parser_sd_img = _subparsers.add_parser(MODE_SD_IMG)
     
-    # MODE_SD - Required arguments
-    
-    _parser.add_option('-d', '--device',
-                       help="Device to install",
-                       metavar='<device>',
-                       dest='device')
-    
-    # MODE_LOOPBACK - Required arguments
-    
-    _parser.add_option('--image',
+    _parser_sd_img.add_argument('--image',
                        help="The filename of the image to create in workdir",
                        metavar='<image>',
                        dest='image')
     
-    _parser.add_option('--image-size',
+    _parser_sd_img.add_argument('--image-size',
                        help="Size in MB of the image file to create (integer" \
                        " number)",
                        metavar='<imagesize_mb>',
                        dest='imagesize_mb')
+
+def _parse_sd_args():
+    global _parser_sd
+    _parser_sd = _subparsers.add_parser(MODE_SD)
+
+    _parser_sd.add_argument('--mmap-file',
+                       help='Memory map config file',
+                       metavar='<mmap>',
+                       dest='mmap_file')
+
+    _parser_sd.add_argument('--device',
+                       help="Device to install",
+                       metavar='<device>',
+                       dest='device',
+                       required=True)
+    
+    _parser_sd.add_argument('--kernel-img',
+                       help='Path to the Kernel Image file to be installed.',
+                       metavar='<kernel_img>',
+                       dest='kernel_img',
+                       required=True)
     
     # MODE_SD and MODE_LOOPBACK - Required arguments
     
-    _parser.add_option('--uflash',
+    _parser_sd.add_argument('--uflash',
                        help='Path to the uflash tool',
                        metavar='<uflash>',
-                       dest='uflash_bin')
+                       dest='uflash_bin',
+                       required=True)
     
-    _parser.add_option('--ubl-file',
+    _parser_sd.add_argument('--ubl-file',
                        help='Path to the UBL file',
                        metavar='<ubl_file>',
-                       dest='ubl_file')
+                       dest='ubl_file',
+                       required=True)
     
-    _parser.add_option('--uboot-file',
+    _parser_sd.add_argument('--uboot-file',
                        help='Path to the U-Boot file',
                        metavar='<uboot_file>',
-                       dest='uboot_file')
+                       dest='uboot_file',
+                       required=True)
     
-    _parser.add_option('--uboot-entry-addr',
+    _parser_sd.add_argument('--uboot-entry-addr',
                        help='U-Boot entry address (decimal)',
                        metavar='<uboot_entry_addr>',
-                       dest='uboot_entry_addr')
+                       dest='uboot_entry_addr',
+                       required=True)
     
-    _parser.add_option('--uboot-load-addr',
+    _parser_sd.add_argument('--uboot-load-addr',
                        help='U-Boot load address (decimal)',
                        metavar='<uboot_load_addr>',
-                       dest='uboot_load_addr')
+                       dest='uboot_load_addr',
+                       required=True)
     
-    _parser.add_option('--uboot-bootargs',
+    _parser_sd.add_argument('--uboot-bootargs',
                        help='U-Boot bootargs environment variable (passed to" \
                        " the Linux kernel)',
                        metavar='<uboot_bootargs>',
-                       dest='uboot_bootargs')
+                       dest='uboot_bootargs',
+                       required=True)
     
-    _parser.add_option('--work-dir',
+    _parser_sd.add_argument('--work-dir',
                        help='Directory to perform temporary operations',
                        metavar='<workdir>',
-                       dest='workdir')
+                       dest='workdir',
+                       required=True)
     
-    # MODE_SD and MODE_LOOPBACK - Optional arguments
-    
-    _parser.add_option('--rootfs',
+    _parser_sd.add_argument('--rootfs',
                        help='Path to the rootfs that will be installed.',
                        metavar='<rootfs>',
                        dest='rootfs',
                        default=None)
+
+def _parse_nand_args():
+    global _parser_nand
+    _parser_nand = _subparsers.add_parser(MODE_NAND)
     
-    # Parse
+    _parser_nand.add_argument('--mmap-file',
+                       help='Memory map config file',
+                       metavar='<mmap>',
+                       dest='mmap_file')
     
-    _options = _parser.get_options()
-    
-    # Check verbose
-    
-    if _options.verbose:
+    components = [COMP_IPL, COMP_BOOTLOADER, COMP_KERNEL, COMP_FS,
+                    COMP_CMDLINE, COMP_BOOTCMD]
+    components_help = ("Components: % s" %
+                       ''.join('%s|' % comp for comp in components).rstrip("|"))
+    _parser_nand.add_argument('--component',
+                       help=components_help,
+                       metavar='<mode>',
+                       dest='installation_mode',
+                       required=False,
+                       choices=components)
+
+def _check_args():
+    global _args
+    _args = _parser.parse_args()
+    if _args.verbose:
         _logger.setLevel(rrutils.logger.DEBUG)
-    
-    # Check quiet (takes precedence over verbose)
-    
-    if _options.quiet:
+    if _args.quiet: # quiet has precedence over verbose
         _logger.setLevel(rrutils.logger.CRITICAL)
     
-    # Check mmap file
+def _check_args_sd():    
+    global _args_sd
+    _args_sd = _parser_sd.parse_args()    
+    _check_is_file(_args_sd.mmap_file)
+    _check_is_file(_args_sd.uflash_bin)
+    _check_x_ok(_args_sd.uflash_bin)
+    _check_is_file(_args_sd.ubl_file)
+    _check_is_file(_args_sd.uboot_file)
+    _check_is_dir(_args_sd.workdir)
+    if _args_sd.rootfs:
+        _check_is_dir(_args_sd.rootfs)
     
-    if not os.path.isfile(_options.mmap_file):
-        _logger.error('Unable to find %s.' % _options.mmap_file)
-        _clean_exit(-1)
-    
-    # Check MODE_SD required arguments
-    
-    if _options.installation_mode == MODE_SD:
-        
-        if not _options.device:
-            _missing_arg_exit('-d/--device')
-        else:
-            # Clean the device string    
-            _options.device = _options.device.rstrip('/')
-    
-    # Check LOOPBACK required arguments
-    
-    if _options.installation_mode == MODE_LOOPBACK:
-        
-        if not _options.image: _missing_arg_exit('--image')
-        
-        if not _options.imagesize_mb:
-            _missing_arg_exit('--image-size')
-        else:
-            try:
-                int(_options.imagesize_mb)
-            except:
-                _missing_arg_exit('--image-size, must be an integer')
-    
-    # Check MODE_SD or MODE_LOOPBACK required arguments
-    
-    if _options.installation_mode == MODE_SD or _options.installation_mode == MODE_LOOPBACK:
-            
-        if not _options.uflash_bin:
-            _missing_arg_exit('--uflash')
-        else:
-            if not os.path.isfile(_options.uflash_bin):
-                _logger.error('Unable to find %s' % _options.uflash_bin)
-                _clean_exit(-1)
-            elif not os.access(_options.uflash_bin, os.X_OK):
-                _logger.error('No execute permissions on %s' % _options.uflash_bin)
-                _clean_exit(-1)
-            
-        if not _options.ubl_file:
-            _missing_arg_exit('--ubl-file')
-        else:
-            if not os.path.isfile(_options.ubl_file):
-                _logger.error('Unable to find %s' % _options.ubl_file)
-                _clean_exit(-1)
-            
-        if not _options.uboot_file:
-            _missing_arg_exit('--uboot-file')
-        else:
-            if not os.path.isfile(_options.uboot_file):
-                _logger.error('Unable to find %s' % _options.uboot_file)
-                _clean_exit(-1)
-            
-        if not _options.uboot_entry_addr:
-            _missing_arg_exit('--uboot-entry-addr')
-        
-        if not _options.uboot_load_addr:
-            _missing_arg_exit('--uboot-load-addr')
-            
-        if not _options.uboot_bootargs:
-            _missing_arg_exit('--uboot-bootargs')
-        
-        if not _options.workdir:
-            _missing_arg_exit('--work-dir')
-        else:
-            if not os.path.isdir(_options.workdir):
-                _logger.error('Unable to find %s' % _options.workdir)
-                _clean_exit(-1)
-    
-        # Check MODE_SD or MODE_LOOPBACK optional arguments
-        
-        if _options.rootfs:
-            if not os.path.isdir(_options.rootfs):
-                _logger.error('Unable to find %s' % _options.rootfs)
-                _clean_exit(-1)
+def _check_args_sd_img():
+    global _args_sd_img
+    _args_sd_img = _parser_sd_img.parse_args()
+    _check_is_file(_args_sd_img.mmap_file)
+    _check_is_file(_args_sd_img.uflash_bin)
+    _check_x_ok(_args_sd_img.uflash_bin)
+    _check_is_file(_args_sd_img.ubl_file)
+    _check_is_file(_args_sd_img.uboot_file)
+    _check_is_dir(_args_sd_img.workdir)
+    if _args.rootfs:
+        _check_is_dir(_args.rootfs)
+    _check_is_int(_args.imagesize_mb, '--image-size')
 
 # ==========================================================================
 # Main logic
@@ -373,47 +353,52 @@ def main():
 
     _init_logging()
     _parse_args()
-
-    if (_options.installation_mode == MODE_SD or
-        _options.installation_mode == MODE_LOOPBACK):
+    _parse_nand_args()
+    _parse_sd_args()
+    _parse_sd_img_args()
+    _check_args()
+    
+    mode = _args.mode
+    
+    if mode == MODE_SD or mode == MODE_LOOPBACK:
         
         # Components installer
         
         comp_installer = methods.sdcard.ComponentInstaller()
-        comp_installer.uflash_bin = _options.uflash_bin
-        comp_installer.ubl_file =_options.ubl_file
-        comp_installer.uboot_file = _options.uboot_file
-        comp_installer.uboot_entry_addr = _options.uboot_entry_addr
-        comp_installer.uboot_load_addr = _options.uboot_load_addr
-        comp_installer.bootargs = _options.uboot_bootargs
-        comp_installer.kernel_image = _options.kernel_file
-        comp_installer.rootfs = _options.rootfs
-        comp_installer.workdir = _options.workdir
+        comp_installer.uflash_bin = _args.uflash_bin
+        comp_installer.ubl_file =_args.ubl_file
+        comp_installer.uboot_file = _args.uboot_file
+        comp_installer.uboot_entry_addr = _args.uboot_entry_addr
+        comp_installer.uboot_load_addr = _args.uboot_load_addr
+        comp_installer.bootargs = _args.uboot_bootargs
+        comp_installer.kernel_image = _args.kernel_img
+        comp_installer.rootfs = _args.rootfs
+        comp_installer.workdir = _args.workdir
         
         # SDCard installer
     
         sd_installer = methods.sdcard.SDCardInstaller(comp_installer)
-        sd_installer.interactive = _options.interactive
-        sd_installer.dryrun = _options.dryrun
+        sd_installer.interactive = _args.interactive
+        sd_installer.dryrun = _args.dryrun
         
-        ret = sd_installer.read_partitions(_options.mmap_file)
+        ret = sd_installer.read_partitions(_args.mmap_file)
         if ret is False: _abort_install()
         
         # Operations
         
-        if _options.installation_mode == MODE_SD:
-            sd_installer.device = _options.device
+        if mode == MODE_SD:
+            sd_installer.device = _args.device
             sd_installer.mode = sd_installer.MODE_SD
             ret = sd_installer.format_sd()
             if ret is False: _abort_install()
-        elif _options.installation_mode == MODE_LOOPBACK:
+        elif mode == MODE_LOOPBACK:
             sd_installer.mode = sd_installer.MODE_LOOPBACK
-            ret = sd_installer.format_loopdevice(_options.workdir +
-                                                 _options.image, 
-                                                 _options.imagesize_mb)
+            ret = sd_installer.format_loopdevice(_args.workdir +
+                                                 _args.image, 
+                                                 _args.imagesize_mb)
             if ret is False: _abort_install()
         
-        ret = sd_installer.mount_partitions(_options.workdir)
+        ret = sd_installer.mount_partitions(_args.workdir)
         if ret is False: _abort_install()
         
         ret = sd_installer.install_components()
@@ -422,11 +407,10 @@ def main():
         ret = sd_installer.release_device()
         if ret is False: _abort_install()
         
-    if _options.installation_mode == MODE_SERIAL:
+    if mode == MODE_NAND:
         pass
             
     _logger.info('Installation complete')
-    
     _clean_exit(0)
     
 if __name__ == '__main__':
