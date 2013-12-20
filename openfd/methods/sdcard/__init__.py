f129cbba595e3ffefa63fb0a3b4a8ee4acb672c7
Jose Pablo Carballo <jose.carballo@ridgerun.com>
2013-12-20 17:25:52 -0600

    Initial support for SdCardExternal installer (sd-script mode) - work in progress

--- openfd/methods/sdcard/__init__.py
+++ openfd/methods/sdcard/__init__.py
@@ -1,3 +1,4 @@
 from sdcard import *
+from sdcard_external import *
 from component import *
 from args import *
---
+++ openfd/methods/sdcard/sdcard_external.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# ==========================================================================
+#
+# Copyright (C) 2012-2013 RidgeRun, LLC (http://www.ridgerun.com)
+# All Rights Reserved.
+#
+# Authors: Jose Pablo Carballo <jose.carballo@ridgerun.com>
+#          Diego Benavides <diego.benavides@ridgerun.com>
+#
+# The contents of this software are proprietary and confidential to RidgeRun,
+# LLC.  No part of this program may be photocopied, reproduced or translated
+# into another programming language without prior written consent of 
+# RidgeRun, LLC.
+#
+# SD-card operations related to the installation of a script capable of
+# programming flash memory.
+#
+# ==========================================================================
+
+# ==========================================================================
+# Imports
+# ==========================================================================
+
+from sdcard import SDCardInstaller
+
+# ==========================================================================
+# Public classes
+# ==========================================================================
+
+class SDCardExternalInstaller(SDCardInstaller):
+    
+    def __init__(self, comp_installer, device='', dryrun=False,
+                 interactive=True, enable_colors=True):
+        """
+        :param comp_installer: :class:`ComponentInstaller` instance.
+        :param device: Device name (i.e. '/dev/sdb').
+        :param dryrun: Enable dryrun mode. Systems commands will be logged,
+            but not executed.
+        :type dryrun: boolean
+        :param interactive: Enable interactive mode. The user will
+            be prompted before executing dangerous commands.
+        :type interactive: boolean
+        """
+        
+        SDCardInstaller.__init__(self, comp_installer, device, dryrun,
+                                 interactive, enable_colors)
+    
+    def install_components(self):
+        SDCardInstaller.install_components(self)
+    
