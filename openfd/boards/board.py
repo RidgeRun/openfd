f685c41f0c71550ccb082e38ff0ed7eddf53e26e
Jose Pablo Carballo <jose.carballo@ridgerun.com>
2014-02-25 09:06:10 -0600

    Defining the Board interface we know so far

--- openfd/boards/board.py
+++ openfd/boards/board.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # ==========================================================================
 #
-# Copyright (C) 2013 RidgeRun, LLC (http://www.ridgerun.com)
+# Copyright (C) 2013-2014 RidgeRun, LLC (http://www.ridgerun.com)
 #
 # Author: Jose Pablo Carballo <jose.carballo@ridgerun.com>
 #
@@ -19,3 +19,19 @@
 
 class Board(object):
     """Board base class."""
+
+    def comp_name(self, comp):
+        raise NotImplementedError
+    
+    def erase_cmd(self):
+        raise NotImplementedError
+    
+    def pre_write_cmd(self, comp):
+        raise NotImplementedError
+    
+    def write_cmd(self, comp):
+        raise NotImplementedError
+    
+    def post_write_cmd(self, comp):
+        raise NotImplementedError
+        
