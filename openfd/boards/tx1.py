a811571c6d99818cfa242d7b701431badc8c3a5c
Manuel <manuel.leiva@ridgerun.com>
2016-08-19 07:51:41 -0600

    Add openfd TX1 configuration files

---
+++ openfd/boards/tx1.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+# ==========================================================================
+#
+# Copyright (C) 2013-2014 RidgeRun, LLC (http://www.ridgerun.com)
+#
+# Author: Jose Pablo Carballo <jose.carballo@ridgerun.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Definitions for the NVIDIA JETSON TX1
+#
+# ==========================================================================
+
+from board import Board
+from tx1_args import Tx1ArgsParser
+from tx1_sd_comp import Tx1SdCompInstaller
+from openfd.methods.board import TftpRamLoader
+
+BOARD_NAME = 'tx1'
+
+# Supported modes
+MODE_SD = 'sd'
+MODE_SD_IMG = 'sd-img'
+MODE_SD_SCRIPT = 'sd-script'
+MODE_SD_SCRIPT_IMG = 'sd-script-img'
+MODE_NAND = 'nand'
+MODE_RAM = 'ram'
+MODE_ENV = 'env'
+
+# Supported modes uboot communication
+MODE_UBOOT = 'uboot_comm'
+
+# Supported components
+COMP_IPL = 'ipl'
+COMP_DTB = 'dtb'
+COMP_BOOTLOADER = 'bootloader'
+COMP_KERNEL = 'kernel'
+COMP_FS = 'fs'
+
+class Tx1(Board):
+    
+    MODES = [MODE_SD, MODE_SD_IMG, MODE_SD_SCRIPT, MODE_SD_SCRIPT_IMG,
+             MODE_NAND, MODE_RAM, MODE_ENV]
+    COMPONENTS = [COMP_IPL, COMP_BOOTLOADER, COMP_DTB, COMP_KERNEL, COMP_FS]
+    
+    mach_description = "TX1 Board"
+    nand_block_size = 131072
+    nand_page_size = 2048
+    mkimage_arch = 'arm'
+
+    def __init__(self, dryrun=False):
+        """
+        :param dryrun: Enable dryrun mode. Systems commands will be logged,
+            but not executed.
+        :type dryrun: boolean
+        """
+        self._parser = Tx1ArgsParser()
+        self._comp_installer = None
+        self._dryrun = dryrun 
+    
+    @property
+    def parser(self):
+        """CLI args parser."""
+        return self._parser
+    @property
+    def sd_comp_installer(self):
+        """Component installer for sdcard method."""
+        return self._comp_installer
+
+    def __set_dryrun(self, dryrun):
+        self._dryrun = dryrun
+        if self._comp_installer:
+            self._comp_installer.dryrun = dryrun
+    
+    def __get_dryrun(self):
+        return self._dryrun
+    
+    dryrun = property(__get_dryrun, __set_dryrun,
+                      doc="""Enable dryrun mode. Systems commands will be
+                     logged, but not executed.""")
+
+    def _check_comp(self, comp):
+        if comp not in self.COMPONENTS:
+            raise AttributeError('Unknown component: %s' % comp)
+
+    def comp_name(self, comp):
+        self._check_comp(comp)
+        if comp is COMP_IPL: return 'ubl'
+        elif comp is COMP_BOOTLOADER: return 'uboot'
+        elif comp is COMP_DTB: return 'dtb'
+        elif comp is COMP_KERNEL: return 'kernel'
+        elif comp is COMP_FS: return 'rootfs'
+
+    def erase_cmd(self, comp):
+        self._check_comp(comp)
+        return 'nand erase'
+    
+    def pre_write_cmd(self, comp):
+        self._check_comp(comp)
+        return ''
+    
+    def write_cmd(self, comp):
+        self._check_comp(comp)
+        if comp is COMP_IPL or comp is COMP_BOOTLOADER:
+            return 'nand write.ubl'
+        else:
+            return 'nand write'
+    
+    def post_write_cmd(self, comp):
+        self._check_comp(comp)
+        return ''
+
+    def add_args(self, parser):
+	subparsers = parser.add_subparsers(help="installation mode (help available)", dest="mode")
+
+        parser_sd = subparsers.add_parser(MODE_SD)
+        parser_sd_img = subparsers.add_parser(MODE_SD_IMG)
+        parser_sd_script = subparsers.add_parser(MODE_SD_SCRIPT)
+        parser_sd_script_img = subparsers.add_parser(MODE_SD_SCRIPT_IMG)
+        parser_ram = subparsers.add_parser(MODE_RAM)
+        parser_env = subparsers.add_parser(MODE_ENV)
+        parser_nand = subparsers.add_parser(MODE_NAND)
+        
+        subparsers_nand = parser_nand.add_subparsers(help="component (--help available)", dest="component")
+        
+	parser_nand_ipl = subparsers_nand.add_parser(COMP_IPL, help="Initial Program Loader (UBL)")
+        parser_nand_bootloader = subparsers_nand.add_parser(COMP_BOOTLOADER, help="Bootloader (U-Boot)")
+        parser_nand_dtb = subparsers_nand.add_parser(COMP_DTB, help="DTB")
+        parser_nand_kernel = subparsers_nand.add_parser(COMP_KERNEL, help="Kernel")
+        parser_nand_fs = subparsers_nand.add_parser(COMP_FS, help="Filesystem")
+        
+        self._parser.add_args_sd(parser_sd)
+        self._parser.add_args_sd_img(parser_sd_img)
+        self._parser.add_args_sd_script(parser_sd_script)
+        self._parser.add_args_sd_script_img(parser_sd_script_img)
+        self._parser.add_args_nand(parser_nand)
+        self._parser.add_args_nand_ipl(parser_nand_ipl)
+        self._parser.add_args_nand_bootloader(parser_nand_bootloader)
+        self._parser.add_args_nand_kernel(parser_nand_dtb)
+        self._parser.add_args_nand_kernel(parser_nand_kernel)
+        self._parser.add_args_nand_fs(parser_nand_fs)
+        self._parser.add_args_ram(parser_ram)
+        self._parser.add_args_env(parser_env)
+
+    def check_args(self, args):
+        if args.mode == MODE_SD:
+            self._parser.check_args_sd(args)
+        elif args.mode == MODE_SD_IMG:
+            self._parser.check_args_sd_img(args)
+        elif args.mode == MODE_SD_SCRIPT:
+            self._parser.check_args_sd_script(args)
+        elif args.mode == MODE_SD_SCRIPT_IMG:
+            self._parser.check_args_sd_script_img(args)
+        elif args.mode == MODE_NAND:
+            self._parser.check_args_nand(args)
+        elif args.mode == MODE_RAM:
+            self._parser.check_args_ram(args)
+        elif args.mode == MODE_ENV:
+            self._parser.check_args_env(args)
+
+    def _get_tftp_loader(self, args):
+        tftp_loader = TftpRamLoader(None, args.board_net_mode)
+        tftp_loader.dir = args.tftp_dir
+        tftp_loader.port = args.tftp_port
+        tftp_loader.host_ipaddr = args.host_ip_addr
+        tftp_loader.net_mode = args.board_net_mode
+        if args.board_net_mode == TftpRamLoader.MODE_STATIC:
+            tftp_loader.board_ipaddr = args.board_ip_addr
+            tftp_loader.dryrun = args.dryrun
+        return tftp_loader
+
+    def sd_init_comp_installer(self, args):
+        self._comp_installer = Tx1SdCompInstaller()
+        self._comp_installer.dryrun = self._dryrun
+        self._comp_installer.uboot_file = args.uboot_file
+	self._comp_installer.uboot_spl = args.uboot_spl
+	self._comp_installer.uboot_bs = args.uboot_bs
+	self._comp_installer.uboot_seek = args.uboot_seek
+        self._comp_installer.uboot_load_addr = args.uboot_load_addr
+        self._comp_installer.bootargs = args.uboot_bootargs
+        self._comp_installer.bootscript = args.uboot_bootscript
+        if hasattr(args, 'kernel_file'): # sd-script mode doesn't need this
+            self._comp_installer.kernel_image = args.kernel_file
+	if hasattr(args, 'kernel_file_type'):
+	    self._comp_installer.kernel_file_type = args.kernel_file_type
+        if hasattr(args, 'kernel_devicetree'):
+            self._comp_installer.kernel_devicetree = args.kernel_devicetree
+        if hasattr(args, 'kernel_tftp'):
+            self._comp_installer.kernel_tftp = args.kernel_tftp
+        if hasattr(args, 'rootfs'): # sd-script mode doesn't need this
+            self._comp_installer.rootfs = args.rootfs
+        self._comp_installer.workdir = args.workdir
+        if args.kernel_tftp:
+            self._comp_installer.tftp_loader = self._get_tftp_loader(args)
+            self._comp_installer.tftp_loader.check_tftp_settings()
+
+
+    def sd_install_components(self, sd):
+        self._comp_installer.install_sd_components(sd)
+
+    def ld_install_components(self, ld):
+        self._comp_installer.install_ld_components(ld)
+        
+    def sd_install_components_external(self, sd):
+        self._comp_installer.install_sd_components_external(sd)
+
+    def ld_install_components_external(self, ld):
+        self._comp_installer.install_ld_components_external(ld)
---
+++ openfd/boards/tx1_sd_comp.py
@@ -0,0 +1,447 @@
+
+#!/usr/bin/env python
+# ==========================================================================
+#
+# Copyright (C) 2013-2014 RidgeRun, LLC (http://www.ridgerun.com)
+#
+# Authors: Jose Pablo Carballo <jose.carballo@ridgerun.com>
+#          Diego Benavides <diego.benavides@ridgerun.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# Component-related operations
+#
+# ==========================================================================
+
+# ==========================================================================
+# Imports
+# ==========================================================================
+
+import os
+import openfd.utils as utils
+import openfd.utils.hexutils as hexutils
+from openfd.storage import SDCardPartition
+from openfd.storage import LoopDevicePartition
+from board import BoardError
+
+# ==========================================================================
+# Public Classes
+# ==========================================================================
+
+class Tx1SdCompInstaller(object):
+    """
+    Class to handle components-related operations.
+    """
+    
+    def __init__(self, dryrun=False):
+        """
+        :param dryrun: Enable dryrun mode. Systems commands will be logged,
+            but not executed.
+        :type dryrun: boolean
+        """
+        
+        self._l = utils.logger.get_global_logger()
+        self._e = utils.executer.get_global_executer()
+        self._workdir = None
+        self._uboot_file = None
+        self._uboot_load_addr = None
+        self._bootargs = None
+        self._bootscript = None
+        self._kernel_image = None
+        self._kernel_devicetree = None
+        self._kernel_tftp = False
+        self._tftp_loader = None
+        self._rootfs = None
+        self._dryrun = dryrun
+        self._e.dryrun = dryrun
+	self._uboot_spl = None
+	self._uboot_bs = None
+	self._uboot_seek = None
+	self._kernel_file_type = None
+
+    def __set_uboot_spl(self, uboot_spl):
+	self._uboot_spl = uboot_spl
+
+    def __get_uboot_spl(self):
+	return self._uboot_spl
+
+    uboot_spl = property(__get_uboot_spl, __set_uboot_spl,
+                          doc="""Path to the SPL file.""")
+
+    def __set_uboot_bs(self, uboot_bs):
+	self._uboot_bs = uboot_bs
+
+    def __get_uboot_bs(self):
+	return self._uboot_bs
+
+    uboot_bs = property(__get_uboot_bs, __set_uboot_bs,
+			doc="""Byte value""")
+
+    def __set_uboot_seek(self, uboot_seek):
+	self._uboot_seek = uboot_seek
+
+    def __get_uboot_seek(self):
+	return self._uboot_seek
+
+    uboot_seek = property(__get_uboot_seek, __set_uboot_seek,
+			   doc="""Seek value""")
+
+    def __set_dryrun(self, dryrun):
+        self._dryrun = dryrun
+        self._e.dryrun = dryrun
+    
+    def __get_dryrun(self):
+        return self._dryrun
+    
+    dryrun = property(__get_dryrun, __set_dryrun,
+                      doc="""Enable dryrun mode. Systems commands will be
+                     logged, but not executed.""")
+
+    def __set_tftp_loader(self, loader):
+        self._tftp_loader = loader
+    
+    def __get_tftp_loader(self):
+        return self._tftp_loader
+    
+    tftp_loader = property(__get_tftp_loader, __set_tftp_loader)
+
+    def __set_uboot_file(self, uboot_file):
+        self._uboot_file = uboot_file
+        
+    def __get_uboot_file(self):
+        return self._uboot_file
+    
+    uboot_file = property(__get_uboot_file, __set_uboot_file,
+                          doc="""Path to the uboot file.""")
+        
+    def __set_uboot_load_addr(self, uboot_load_addr):
+        if hexutils.is_valid_addr(uboot_load_addr):
+            self._uboot_load_addr = uboot_load_addr
+        else:
+            self._l.error('Invalid u-boot load address: %s' % uboot_load_addr)
+            self._uboot_load_addr = None
+        
+    def __get_uboot_load_addr(self):
+        return self._uboot_load_addr
+    
+    uboot_load_addr = property(__get_uboot_load_addr, __set_uboot_load_addr,
+                               doc="""Uboot load address, in decimal or
+                                hexadecimal (`'0x'` prefix).""")
+    
+    def __set_bootargs(self,bootargs):
+        self._bootargs = bootargs
+    
+    def __get_bootargs(self):
+        return self._bootargs
+    
+    bootargs = property(__get_bootargs, __set_bootargs,
+                        doc="""Uboot environment variable 'bootargs'.""")
+
+    def __set_bootscript(self,bootscript):
+        self._bootscript = bootscript
+    
+    def __get_bootscript(self):
+        return self._bootscript
+    
+    bootscript = property(__get_bootscript, __set_bootscript,
+                        doc="""Path to the bootscript file.""")
+
+    def __set_kernel_image(self, kernel_image):
+        self._kernel_image = kernel_image
+        
+    def __get_kernel_image(self):
+        return self._kernel_image
+    
+    kernel_image = property(__get_kernel_image, __set_kernel_image,
+                            doc="""Path to the kernel image.""")
+
+    def __set_kernel_file_type(self, kernel_file_type):
+	self._kernel_file_type = kernel_file_type
+
+    def __get_kernel_file_type(self):
+	return self._kernel_file_type
+
+    kernel_file_type = property(__get_kernel_file_type, __set_kernel_file_type,
+				doc="""Type of kernel image""")
+
+    def __set_kernel_devicetree(self, kernel_devicetree):
+        self._kernel_devicetree = kernel_devicetree
+        
+    def __get_kernel_devicetree(self):
+        return self._kernel_devicetree
+    
+    kernel_devicetree = property(__get_kernel_devicetree, __set_kernel_devicetree,
+                            doc="""Path to the kernel devicetree(.dtb) file.""")
+
+    def __set_kernel_tftp(self, kernel_tftp):
+        self._kernel_tftp = kernel_tftp
+        
+    def __get_kernel_tftp(self):
+        return self._kernel_tftp
+    
+    kernel_tftp = property(__get_kernel_tftp, __set_kernel_tftp,
+                            doc="""Enable kernel tftp.""")
+    
+    def __set_rootfs(self, rootfs):
+        self._rootfs = rootfs
+
+    def __get_rootfs(self):
+        return self._rootfs
+    
+    rootfs = property(__get_rootfs, __set_rootfs,
+                      doc="""Path to the rootfs directory. Set to None if this
+            installation does not require a rootfs, i.e. NFS will be used.""")
+    
+    def __set_workdir(self, workdir):
+        self._workdir = workdir
+        
+    def __get_workdir(self):
+        return self._workdir
+    
+    workdir = property(__get_workdir, __set_workdir,
+               doc="""Path to the workdir - a directory where this installer
+               can write files and perform other temporary operations.""")
+    
+    def install_uboot(self, device):
+        """
+        Flashes  uboot to the given device, using dd.
+        
+        This method needs`, 
+        :attr:`uboot_file` to be already set.
+	:attr:`uboot_spl` to be already set.
+	:attr:`uboot_seek` to be already set.
+	:attr:`uboot_bs` to be already set.
+        
+        :param device: Device where to flash UBL and uboot (i.e. '/dev/sdb').
+        :exception BoardError: On error.
+        """
+        
+        self._l.info('Installing uboot')
+        cmd = ('sudo dd' + 
+              ' if=' + self._uboot_file + 
+              ' of=' + device +
+               ' seek=' + self._uboot_seek + ' bs=' + self._uboot_bs ) #The default values are: seek=2 bs=512
+        if self._e.check_call(cmd) != 0:
+            raise BoardError('Failed to flash uboot into %s' % device)
+	
+	if self._uboot_spl != None:
+	    cmd = ('sudo dd' + 
+		' if=' + self._uboot_spl + 
+		' of=' + device + 
+		' seek=1 bs=1K')
+	    if self._e.check_call(cmd) != 0:
+		raise BoardError('Failed to flash SPL into %s' % device)
+    
+    def install_uboot_env(self, mount_point):
+        """
+        Installs the uboot environment (uEnv.txt) to the given mount point.
+        
+        This methods needs :attr:`uboot_load_addr` and :attr:`workdir`
+        to be already set.
+        
+        :param mount_point: Path where to install the uboot environment.
+        :exception BoardError: On error.
+        """
+        
+        self._l.info('Installing uboot environment')
+        uboot_load_addr = hexutils.str_to_hex(self._uboot_load_addr)
+        uenv_file = os.path.join(self._workdir, "uEnv.txt")
+        if not self._dryrun:
+            with open(uenv_file, "w") as uenv:
+                bootargs = 'bootargs=%s' % self._bootargs.strip()
+                if self._kernel_tftp:
+                    loadtftpimage = 'loadtftpimage=%s'% self._tftp_loader.get_env_load_file_to_ram(
+                        self._kernel_image, uboot_load_addr)
+                    self._l.debug("  uEnv.txt <= '%s'" % loadtftpimage)
+                    uenv.write("%s\n" % loadtftpimage)
+                    kernel_load = 'run loadtftpimage'
+                else:
+                    kernel_load = 'run loaduimage'
+                    
+                uenvcmd = ('uenvcmd=echo Running uenvcmd ...; %s; ' % kernel_load)
+                if self._kernel_devicetree:
+                    uenvcmd = uenvcmd + ' run loadfdt; bootm ${loadaddr} - ${fdt_addr}'
+                else:
+                    uenvcmd = uenvcmd + ' bootm'
+
+                self._l.debug("  uEnv.txt <= '%s'" % bootargs)
+                uenv.write("%s\n" % bootargs)
+                self._l.debug("  uEnv.txt <= 'autostart=no")
+                uenv.write("autostart=no\n")
+                self._l.debug("  uEnv.txt <= '%s'" % uenvcmd)
+                uenv.write("%s\n" % uenvcmd)
+        cmd = 'sudo cp %s %s' % (uenv_file, mount_point)
+        if self._e.check_call(cmd) != 0:
+            raise BoardError('Failed to install uboot env file.')
+
+    def install_uboot_bootscript(self, mount_point):
+        """
+        Installs the uboot script (bootscript) to the given mount point.
+        
+        This methods needs :attr:`uboot_bootscript` to be already set.
+        
+        :param mount_point: Path where to install the uboot script.
+        :exception BoardError: On error.
+        """
+
+        if self._bootscript:
+            self._l.info('Installing uboot script')
+
+            cmd = 'sudo cp %s %s/' % (self._bootscript, mount_point)
+            if self._e.check_call(cmd) != 0:
+                raise BoardError('Failed copying %s to %s' %
+                                 (self._bootscript, mount_point))
+        else:
+            self._l.warning('No bootscript file, omitting...')
+
+
+    def install_kernel(self, mount_point):
+        """
+        Installs the kernel image on the given mount point.
+        
+        This methods needs :attr:`kernel_image` to be already set.
+        
+        :param mount_point: Path to where install the kernel image.
+        :exception BoardError: On error.
+        """
+
+        self._l.info('Installing kernel')
+        # Create /boot directory if doesn't exist
+        cmd = 'sudo mkdir -p %s/boot/' % (mount_point)
+        if self._e.check_call(cmd) != 0:
+            raise BoardError('Failed creating directory %s/boot' %
+                               (mount_point))
+        
+        cmd = 'sudo cp %s %s/boot/%s' % (self._kernel_image, mount_point, self.kernel_file_type)
+        if self._e.check_call(cmd) != 0:
+            raise BoardError('Failed copying %s to %s' %
+                               (self._kernel_image, mount_point))
+            
+    def install_kernel_devicetree(self, mount_point):
+        """
+        Installs the kernel devicetree on the given mount point.
+        
+        This methods needs :attr:`kernel_devicetree` to be already set.
+        
+        :param mount_point: Path to where install the kernel image.
+        :exception BoardError: On error.
+        """
+        
+        self._l.info('Installing kernel devicetree')
+        # Create /boot directory if doesn't exist
+        cmd = 'sudo mkdir -p %s/boot/' % (mount_point)
+        if self._e.check_call(cmd) != 0:
+            raise BoardError('Failed creating directory %s/boot' %
+                               (mount_point))
+        
+        cmd = 'sudo cp %s %s/boot/' % (self._kernel_devicetree, mount_point)
+        if self._e.check_call(cmd) != 0:
+            raise BoardError('Failed copying %s to %s' %
+                             (self._kernel_devicetree, mount_point))
+
+    def install_rootfs(self, mount_point):
+        """
+        If any, installs :attr:`rootfs` to the given mount point.
+        
+        :param mount_point: Path to where install rootfs.
+        :exception BoardError: On error.
+        """
+                                 
+        if self._rootfs:
+            self._l.info('Installing rootfs (this may take a while)')
+            cmd = 'cd %s ; find . | sudo cpio -pdum %s' % (self._rootfs,
+                                                           mount_point)
+            if self._e.check_call(cmd) != 0:
+                raise BoardError('Failed installing rootfs '
+                                              'into %s' % mount_point)
+            if self._e.check_call('sync') != 0:
+                raise BoardError('Unable  to sync')
+        else:
+            self._l.warning('No directory for "%s", omitting...'
+                                        % (SDCardPartition.COMPONENT_ROOTFS))
+
+
+    def install_sd_components(self, sd):
+        """
+        Installs the specified components for each partition.
+        
+        :exception BoardError: On failure installing the components.
+        """
+        
+        i = 1
+        for part in sd.partitions:
+            cmd = 'mount | grep %s  | cut -f 3 -d " "' % sd.partition_name(i)
+            output = self._e.check_output(cmd)[1]
+            mount_point = output.replace('\n', '')
+            for comp in part.components:
+                if comp == SDCardPartition.COMPONENT_BOOTLOADER:
+                    self.install_uboot(sd.name)
+                    self.install_uboot_env(mount_point)
+                    self.install_uboot_bootscript(mount_point)
+                elif comp == SDCardPartition.COMPONENT_KERNEL:
+                    if not self._kernel_tftp:
+                        self.install_kernel(mount_point)
+                    if self._kernel_devicetree:
+                        self.install_kernel_devicetree(mount_point)
+                elif comp == SDCardPartition.COMPONENT_ROOTFS:
+                    self.install_rootfs(mount_point)
+                else:
+                    raise BoardError('Invalid component: %s' % comp)
+            i += 1
+
+    def install_ld_components(self, ld):
+        """
+        Installs the specified components for each partition.
+        
+        :exception BoardError: On failure installing the components.
+        """
+        
+        for part in ld.partitions:
+            cmd = 'mount | grep %s  | cut -f 3 -d " "' % part.device
+            output = self._e.check_output(cmd)[1]
+            mount_point = output.replace('\n', '')
+            for comp in part.components:
+                if comp == LoopDevicePartition.COMPONENT_BOOTLOADER:
+                    self.install_uboot(ld.name)
+                    self.install_uboot_env(mount_point)
+                    self.install_uboot_bootscript(mount_point)
+                elif comp == LoopDevicePartition.COMPONENT_KERNEL:
+                    self.install_kernel(mount_point)
+                    if self._kernel_devicetree:
+                        self.install_kernel_devicetree(mount_point)
+                elif comp == LoopDevicePartition.COMPONENT_ROOTFS:
+                    self.install_rootfs(mount_point)
+                else:
+                    raise BoardError('Invalid component: %s' % comp)
+                
+    def install_sd_components_external(self, sd):
+        """
+        Installs the specified components for each partition, as required
+        in external (sd-script) mode.
+        
+        :exception BoardError: On failure installing the components.
+        """
+        
+        for part in sd.partitions:
+            for comp in part.components:
+                if comp == SDCardPartition.COMPONENT_BOOTLOADER:
+                    self.install_uboot(sd.name)
+                else:
+                    raise BoardError('Invalid component: %s' % comp)
+
+    def install_ld_components_external(self, ld):
+        """
+        Installs the specified components for each partition, as required
+        in external (sd-script) mode.
+        
+        :exception BoardError: On failure installing the components.
+        """
+        
+        for part in ld.partitions:
+            for comp in part.components:
+                if comp == LoopDevicePartition.COMPONENT_BOOTLOADER:
+                    self.install_uboot(ld.name)
+                else:
+                    raise BoardError('Invalid component: %s' % comp)
